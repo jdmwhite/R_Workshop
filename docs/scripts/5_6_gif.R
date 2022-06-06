7e41d04c3c201da7ebb2fd55f73c86701af4f309
Joseph White <josephwhite16@gmail.com>
2022-06-07 01:08:24 +0200

    spelling error

--- docs/scripts/5_6_gif.R
+++ docs/scripts/5_6_gif.R
@@ -5,14 +5,14 @@ install.packages('magick')
 library(magick)
 
 #### Load in the files ----
-imgs <- list.files('output/figs/animation/', full.names = TRUE)
+imgs <- list.files('output/figs/animation', full.names = TRUE)
 img_list <- lapply(imgs, image_read)
 
 #### Join the images together ----
 img_joined <- image_join(img_list)
 
 #### Animate the images ----
-img_animated <- image_animate(img_joined, fps = 2)
+img_animated <- image_animate(img_joined, fps = 1)
 img_animated
 
 #### Save the animation to file as a gif ----
